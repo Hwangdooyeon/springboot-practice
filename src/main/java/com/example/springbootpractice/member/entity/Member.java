1f1b62c10bf61d8a0499a1129666bce8e74b0c5d
hwang <yueuch102@naver.com>
2023-06-26 17:06:56 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootpractice/member/entity/Member.java
@@ -0,0 +1,40 @@
+package com.example.springbootpractice.member.entity;
+
+
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="member",
+        uniqueConstraints = {@UniqueConstraint(
+                name = "email_unique",
+                columnNames = {"email"} )
+        })
+@DynamicUpdate
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    private String role;
+
+    }
+
+
